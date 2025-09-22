01f4c3fae71a0acf0669e1f91da662e73e3c735f
HimanshuTiwri <himanshutiwari1061@gmail.com>
2025-09-22 17:59:24 +0530

    completed all modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+AWS_S3_BUCKET_NAME="wafer_fault"
+MONGO_DATABASE_NAME="sensor_monitoring"
+MONGO_COLLECTION_NAME="waferfault"
+
+
+
+TARGET_COLUMN="quality"
+MONGO_DB_URL="mongodb+srv://himanshutiwari1061:pranav1061@cluster0.qoxh9z2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+MODEL_FILE_NAME="model"
+MODEL_FILE_EXTENSION=".pkl"
+
+artifact_folder="artifacts"
